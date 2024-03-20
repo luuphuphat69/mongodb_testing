2181903c26b6eede9e0a1f14465e14eed1a7a5f8
luuphuphat69 <luuphuphat69@gmail.com>
2024-03-20 20:45:51 +0700

    first commit

---
+++ user.test.js
@@ -0,0 +1,55 @@
+const { MongoClient } = require("mongodb");
+const { faker } = require("@faker-js/faker");
+
+jest.setTimeout(30000);
+
+const uri =
+  "mongodb+srv://luuphuphat:adolph29122003@cluster0.nipyiqe.mongodb.net/";
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+describe("Database Tests", () => {
+  let usersCollection;
+
+  beforeAll(async () => {
+    try {
+      await client.connect();
+      const db = client.db("AudioCloud");
+      usersCollection = db.collection("User");
+    } catch (err) {
+      console.error("Error connecting to the database:", err);
+    }
+  });
+
+  // CREATE
+  test("Test CREATE", async () => {
+    let newUsers = [];
+    let total_users_to_add = 3;
+
+    for (let i = 0; i < total_users_to_add; i++) {
+      newUsers.push({
+        UserId: faker.string.uuid(),
+        Account: faker.internet.userName(),
+        Password: faker.internet.password(),
+        Email: faker.internet.email(),
+        isPro: false
+      });
+    }
+    const result = await usersCollection.insertMany(newUsers);
+    expect(result.insertedCount).toBe(total_users_to_add);
+  }, 30000);
+
+
+  //READ
+  test("Test READ", async () => {
+    const findUser = await usersCollection.findOne({ email: 'example@gmail.com' });
+    expect(findUser.name).toBe(sampleUser.name);
+  }, 30000);
+
+  afterAll(async () => {
+    await client.close();
+  });
+});
